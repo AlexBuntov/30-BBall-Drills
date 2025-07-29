912fa977670a3a304176f703cad3e3bb4b61eb11
AlexBunt <Buntov2003@mail.ru>
2025-07-29 18:00:37 +0300

    Add theming

--- app/src/main/java/com/example/a30bballdrills/ui/theme/Type.kt
+++ app/src/main/java/com/example/a30bballdrills/ui/theme/Type.kt
@@ -1,28 +1,42 @@
 package com.example.a30bballdrills.ui.theme
 
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.a30bballdrills.R
 
 // Set of Material typography styles to start with
+val Dmsans = FontFamily(
+    Font(R.font.dmsans_regular),
+    Font(R.font.dmsans_semibold, FontWeight.SemiBold)
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    labelLarge = TextStyle(
+        fontFamily = Dmsans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Dmsans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.sp
     ),
+    bodyMedium = TextStyle(
+        fontFamily = Dmsans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+
+    )
+    /* Other default text styles to override
+
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
